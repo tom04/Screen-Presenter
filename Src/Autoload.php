3438668a72fb524e5aa418aa3ae034e71e37afbd
Georg Ringer <georg.ringer@gmail.com>
2013-04-17 07:14:53 +0200

    [TASK] Small bugfixes
    - extend config
    - fix troubles with unix <> windows

--- Src/Autoload.php
+++ Src/Autoload.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../Vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__ . '/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
-	'NoiseLabs\\ToolKit\\ConfigParser' => __DIR__ . '\\..\\vendor',
+	'NoiseLabs' => __DIR__ . PATH_SEPARATOR . '..' . PATH_SEPARATOR . 'vendor',
 	'ScreenPresenter' => __DIR__
 ));
 
